0fe303c10cde37d43f9c14fb910358bb5a9546d1
Julian David Ortega Solarte <95juliandos@gmail.com>
2025-11-13 12:18:32 -0500

    feat: Last update

--- app/database.py
+++ app/database.py
@@ -10,5 +10,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# autocommit=False Requiere que tú llames manualmente a db.commit()
+# autoflush=False Evita que SQLAlchemy sincronice los cambios con la DB antes de cada query
 
-Base = declarative_base()
+Base = declarative_base() # Clase base para los modelos ORM
